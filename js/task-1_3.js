7c36a6b2ec8a5b55b09754a37c99578d793405b5
Julia-Davydiuk <70822407+Julia-Davydiuk@users.noreply.github.com>
2020-11-25 20:59:11 +0100

    дз 3-4 начало

--- js/task-1_3.js
+++ js/task-1_3.js
@@ -3,15 +3,29 @@
 const ADMIN_PASSWORD = 'jqueryismyjam';
 let message;
 
-const passwordInput = prompt('Введите пароль администратора');
+const passwordInput = prompt('Введите пароль администратора' || 'Отменено пользователем!') ;
 
-if(ADMIN_PASSWORD !== true) {
-    message = 'Отменено пользователем!';
-    console.log(message);
-} else if(ADMIN_PASSWORD === true) {
+
+if(ADMIN_PASSWORD === passwordInput) {
     message = 'Добро пожаловать!';
     console.log(message);
-} else {
-    message = 'Доступ запрещен, неверный пароль!';
-    console.log(message)
-}
+} 
+else  if(ADMIN_PASSWORD !== passwordInput) {
+        message = 'Доступ запрещен, неверный пароль!';
+        console.log(message)
+    } else {
+        message = 'Отменено пользователем!';
+        console.log(message);
+    }
+
+// if(passwordInput === NaN) {
+//     message = 'Отменено пользователем!';
+//     console.log(message);
+// } else 
+// if(ADMIN_PASSWORD === passwordInput) {
+//     message = 'Добро пожаловать!';
+//     console.log(message);
+// } else if(ADMIN_PASSWORD !== passwordInput) {
+// message = 'Доступ запрещен, неверный пароль!';
+// console.log(message)
+// }
--- js/task-1_4.js
+++ js/task-1_4.js
@@ -1,4 +1,25 @@
 const credits = 23580;
 const pricePerDroid = 3000;
+let totalPrice;
 
-const quantityDroid = prompt('Сколько дронов вы хотите купить?');
+const quantityDroid = prompt('Сколько дроидов вы хотите купить?');
+console.log(quantityDroid)
+
+
+// if(quantityDroidt = null) {
+//         message = 'Отменено пользователем!';
+//         console.log(message);
+//     }
+
+totalPrice = quantityDroid * pricePerDroid;
+console.log(totalPrice)
+
+if(totalPrice > credits) {
+    message = 'Недостаточно средств на счету!';
+    console.log(message)
+}
+
+balanceOfCredits = credits - totalPrice;
+message = `Вы купили ${quantityDroid} дроидов, на счету осталось ${balanceOfCredits} кредитов.`;
+console.log(message)
+
