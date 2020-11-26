4a4f10b079518fc189ec27039784d18acd776da7
Julia-Davydiuk <70822407+Julia-Davydiuk@users.noreply.github.com>
2020-11-26 20:46:34 +0100

    закончила 4 задание

--- js/task-1_4.js
+++ js/task-1_4.js
@@ -1,26 +1,19 @@
 const credits = 23580;
 const pricePerDroid = 3000;
 let totalPrice;
-let message;
 
 const answer = prompt('Сколько дроидов вы хотите купить?');
-console.log(answer)
+// console.log(answer)
 
+totalPrice = answer * pricePerDroid;
+// console.log(totalPrice)
 
 if(answer === null) {
     console.log('Отменено пользователем!');
-} else if(totalPrice = answer * pricePerDroid && totalPrice > credits) {
+
+} else if(totalPrice > credits) {
     console.log('Недостаточно средств на счету!');
-} 
-// } else {balanceOfCredits = credits - totalPrice;
-//     console.log(`Вы купили ${answer} дроидов, на счету осталось ${balanceOfCredits} кредитов.`);
-// }
 
-// if(totalPrice > credits) {
-//     message = 'Недостаточно средств на счету!';
-//     console.log(message)
-// }
-
-// balanceOfCredits = credits - totalPrice;
-// message = `Вы купили ${quantityDroid} дроидов, на счету осталось ${balanceOfCredits} кредитов.`;
-// console.log(message)
+} else { const balanceOfCredits = credits - totalPrice;
+    console.log(`Вы купили ${answer} дроидов, на счету осталось ${balanceOfCredits} кредитов.`);
+}
