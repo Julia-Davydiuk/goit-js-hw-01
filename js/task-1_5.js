e839e9f0facff68925424ddac4e6fb6aba4cce30
Julia-Davydiuk <70822407+Julia-Davydiuk@users.noreply.github.com>
2020-12-01 08:29:00 +0100

    изменила задачу 5

--- js/task-1_5.js
+++ js/task-1_5.js
@@ -1,38 +1,43 @@
 let price = 0;
 let result;
-
+let normalizedInput;
 
 const userInput = prompt('Введите страну доставки');
-const normalizedInput = userInput.toLowerCase();
 
-switch(normalizedInput) {
-    case 'китай':
-    result = 'Китай';
-    price = 100;
-    break;
+if(userInput !== null) {
+    normalizedInput = userInput.toLowerCase();
 
-    case 'чили':
-    result = 'Чили';
-    price = 250;
-    break;
-
-    case 'австралия':
-    result = 'Австралия';
-    price = 170;
-    break;
-
-    case 'индия':
-    result = 'Индия';
-    price = 80;
-    break;
-
-    case 'ямайка':
-    result = 'Ямайка';
-    price = 120;
-    break;
-
-    default: 
-    result = 'В вашей стране доставка не доступна.';
-    alert('В вашей стране доставка не доступна.');
+    switch(normalizedInput) {
+            case 'китай':
+            result = 'Китай';
+            price = 100;
+            break;
+        
+            case 'чили':
+            result = 'Чили';
+            price = 250;
+            break;
+        
+            case 'австралия':
+            result = 'Австралия';
+            price = 170;
+            break;
+        
+            case 'индия':
+            result = 'Индия';
+            price = 80;
+            break;
+        
+            case 'ямайка':
+            result = 'Ямайка';
+            price = 120;
+            break;
+        
+            default: 
+            result = 'В вашей стране доставка не доступна.';
+            alert(result);
+        }
+         console.log(`Доставка в ${result} будет стоить ${price} кредитов.`);
+} else {
+    userInput === null
 }
- console.log(`Доставка в ${result} будет стоить ${price} кредитов.`)
